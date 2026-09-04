b2365d307a8d2d36bb0381868d75161e4ef61bfe
nakami2105 <notnakami@protonmail.com>
2026-09-03 23:55:15 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,2 @@
+function openLetter(){document.getElementById('envelope').style.display='none';document.getElementById('letter').classList.remove('hidden');}
+function showSpotify(){document.getElementById('spotify').innerHTML='PEGA AQUI TU IFRAME DE SPOTIFY';}
